294b4f644aed4a7d9e4a6b7b5c0b76a9e71259d3
Mark Jarjour <m.j@sent.com>
2024-01-30 16:44:19 -0500

    Remove TRPC

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,9 @@
 import { appWithTranslation } from "next-i18next";
 import type { AppType } from "next/app";
 import "styles/globals.css";
-import { api } from "utils/api";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
     return <Component {...pageProps} />;
 };
 
-export default api.withTRPC(appWithTranslation(MyApp));
+export default appWithTranslation(MyApp);
